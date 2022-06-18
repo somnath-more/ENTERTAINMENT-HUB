ad699555d2483288f71d794c206e82f090023896
Somnath More <84559245+smore83@users.noreply.github.com>
2022-06-19 00:56:23 +0530

    Add files via upload

---
+++ dada/src/main/java/dada/dada/DadaApplication.java
@@ -0,0 +1,13 @@
+package dada.dada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadaApplication.class, args);
+	}
+
+}
---
+++ dada/src/test/java/dada/dada/DadaApplicationTests.java
@@ -0,0 +1,13 @@
+package dada.dada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
